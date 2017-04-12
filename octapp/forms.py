da4d3c3a5558454117b32493ddcf03ddefc64cfd
Cheltigmashev <danil.cheltigmashev@yandex.ru>
2017-04-12 22:47:26 +0700

    to form and template added checkbox for confirming of reading user's manual; style improvements of registration page

--- octapp/forms.py
+++ octapp/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
 
 from registration import validators
 from registration.forms import RegistrationFormTermsOfService
@@ -10,7 +9,14 @@ from registration.forms import RegistrationFormTermsOfService
 User = get_user_model()
 
 class RegistrationFormTermOfServiceUniqueEmail(RegistrationFormTermsOfService):
+    user_manual = forms.BooleanField(
+        widget=forms.CheckboxInput,
+        label=(u'Я прочитал(а) инструкцию пользователя'),
+        required=True,
+    )
+
     def clean_email(self):
         if User.objects.filter(email__iexact=self.cleaned_data['email']):
             raise forms.ValidationError(validators.DUPLICATE_EMAIL)
         return self.cleaned_data['email']
+
