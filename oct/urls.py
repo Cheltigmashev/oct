b31a7a11c7282954eb6f802cdd978c41e756ef9d
Cheltigmashev <danil.cheltigmashev@yandex.ru>
2017-04-25 10:21:45 +0700

    fixed unnecessary . at 39 of urls.py

--- oct/urls.py
+++ oct/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
 ]
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-urlpatterns += staticfiles_urlpatterns(). 
+urlpatterns += staticfiles_urlpatterns()
 
 try:
     from .local_urls import statics
