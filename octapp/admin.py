eccb7ee854c9d177fa5096f6ff6904c1329c35c3
Cheltigmashev <danil.cheltigmashev@yandex.ru>
2017-05-03 02:11:45 +0700

    all new models have been registered in octapp/admin.py

--- octapp/admin.py
+++ octapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Test, Comment, Category, ResultScale, Tag, Test_rate
+from .models import ClosedQuestion, ClosedQuestionOption, OpenQuestion, SequenceQuestion
+from .models import SequenceQuestionElement, ComparisonQuestion, ComparisonQuestionElement
 
 admin.site.register(Test)
 admin.site.register(Comment)
@@ -7,3 +9,11 @@ admin.site.register(Category)
 admin.site.register(ResultScale)
 admin.site.register(Tag)
 admin.site.register(Test_rate)
+
+admin.site.register(ClosedQuestion)
+admin.site.register(ClosedQuestionOption)
+admin.site.register(OpenQuestion)
+admin.site.register(SequenceQuestion)
+admin.site.register(SequenceQuestionElement)
+admin.site.register(ComparisonQuestion)
+admin.site.register(ComparisonQuestionElement)
