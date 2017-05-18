98496e09c7aa3f3b673fea451186738f91de2e97
Cheltigmashev <danil.cheltigmashev@yandex.ru>
2017-05-18 17:34:19 +0700

    added js-controlling for test passing; input with a reason of failure has been removed

--- octapp/static/octapp/js/test_controlling.js
+++ octapp/static/octapp/js/test_controlling.js
@@ -1,3 +1,17 @@
 $(document).ready(() => {
-
+  passingArea = $('div.passing_area');
+  passingArea.mouseleave(() => {
+      alert('Выход курсора мыши из области прохождения (списка вопросов) заблокировано контролем прохождения, прохождение теста будет принудительно завершено.');
+      document.getElementById('answers_form').submit();
+    });
+  $('body').contextmenu(() => {
+      alert('Нажатие правой клавиши мыши заблокировано контролем прохождения, прохождение теста будет принудительно завершено.');
+      document.getElementById('answers_form').submit();
+    });
+  $('body').keydown((e) => {
+      if (e.keyCode === 17 || e.keyCode === 91 || e.keyCode === 9 || e.keyCode === 18) {
+          alert('Нажатие клавиш ctrl, windows, tab, alt заблокировано контролем прохождения, прохождение теста будет принудительно завершено.');
+          document.getElementById('answers_form').submit();
+        }
+    });
 });
