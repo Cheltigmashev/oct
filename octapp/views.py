70a4ebeb7848d08c582c8d2d223d4980fef1553b
Cheltigmashev <danil.cheltigmashev@yandex.ru>
2017-05-21 07:53:06 +0700

    defect of searching unpublished tests has been fixed

--- octapp/views.py
+++ octapp/views.py
@@ -236,7 +236,7 @@ def tests(request):
 def search(request):
     search_type = request.GET.get('search_type', False)
     if search_type == 'search_in_tests_names':
-        tests = Test.objects.filter(name__icontains=request.GET.get('search')).order_by('name')
+        tests = Test.objects.filter(name__icontains=request.GET.get('search')).filter(published_date__lte=timezone.now()).order_by('name')
     elif search_type == 'search_in_questions':
         matching_questions = []
         matching_questions.append(ClosedQuestion.objects.filter(question_content__icontains=request.GET.get('search')))
@@ -249,6 +249,7 @@ def search(request):
             for question in questions_of_some_type:
                 test_set.add(question.question_of_test.test)
         tests = list(test_set)
+        tests = [test for test in tests if test.published_date > timezone.now()]
         tests.sort(key=lambda i: i.name, reverse=False)
     # Production — 25, 5
     context = get_pagination(int(request.GET.get('page', '1')), tests, 25, 5)
