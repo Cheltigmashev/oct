eb06ed738b487efe77e17a163a7a46d7d3b50927
Cheltigmashev <danil.cheltigmashev@yandex.ru>
2017-05-23 15:16:52 +0700

    SECRET_KEY has been moved to txt file that is imported in settings.py

--- oct/settings.py
+++ oct/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pj@9m&)8+8nez#(8%v+(in4f-3&6(t=q#%or4vyn+z2k9rs9se'
+with open('/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
